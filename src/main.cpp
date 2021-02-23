54b3b92066b83f9464d0deca35a1a352423809df
PeterWinchester <wincsoft_pw@outlook.com>
2021-02-23 17:05:06 +0800

    源文件和头文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,17 @@
+/*
+ * main.cpp
+ * 
+ * This file is a part of TermOSEmulator.
+ * 
+ * Written by Peter Winchester <wincsoft_pw@outlook.com>
+ * Copyright (C) 2021 Peter Winchester.
+ * 
+ * TermOSEmulator is free software, follows the GNU General
+ * Public License.
+ * 
+ * NOTE: TermOSEmulator requires Windows API, so it can only
+ * run on the Windows.
+ * 
+ */
+
 #include "TermOSEmulator.h"
