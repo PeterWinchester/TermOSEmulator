f52719be7c4e6fcf5563689b19719de506123f6a
PeterWinchester <wincsoft_pw@outlook.com>
2021-02-24 16:05:09 +0800

    增加启动界面

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,8 @@ int main(int argc, char const *argv[]) {
   if (!initTermOSEmulator()) {
     printf("Error! Cannot load system data.\n");
     printf("Please check the files in the /dat directory.\n");
+    pause(NULL);
+    return 0;
   }
   startUp();
   while (toseRunning) {
@@ -40,7 +42,14 @@ int initTermOSEmulator() {
 }
 
 void startUp() {
-  
+  printf("Welcome to TermOSEmulator!\n\n");
+  printf("Copyright (C) 2021 Peter Winchester.\n\n");
+  printf("TermOSEmulator is free software, follow the GNU General Public License.\n");
+  printf("See https://www.gnu.org/ for more information about the GPL.\n");
+  printf("Visit https://github.com/PeterWinchester/TermOSEmulator for source code.\n");
+  printf("The writer's email is wincsoft_pw@outlook.com. To report bugs, please\n");
+  printf("visit https://github.com/PeterWinchester/Termosemulator/issues, or you\n");
+  printf("can mail the writer.\n");
 }
 
 int getSystemRootPath() {
